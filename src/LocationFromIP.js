b587cc16945513ac1e0e5b3e28dfa73612f9be45
Brian <briangan157@gmail.com>
2022-02-10 12:44:29 -0500

    option 1 update

---
+++ src/LocationFromIP.js
@@ -0,0 +1,90 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// dependencies / things imported
+import { LitElement, html, css } from 'lit';
+import '@lrnwebcomponents/wikipedia-query/wikipedia-query.js';
+import { UserIP } from './UserIP.js';
+
+export class LocationFromIP extends LitElement {
+  static get tag() {
+    return 'location-from-ip';
+  }
+
+  constructor() {
+    super();
+    this.UserIpInstance = new UserIP();
+    this.locationEndpoint = 'https://freegeoip.app/json/';
+    this.long = null;
+    this.lat = null;
+  }
+
+  static get properties() {
+    return {
+      long: { type: Number, reflect: true },
+      lat: { type: Number, reflect: true },
+      city: { type: String, reflect: true },
+      region_name: { type: String, reflect: true },
+    };
+  }
+
+  firstUpdated(changedProperties) {
+    if (super.firstUpdated) {
+      super.firstUpdated(changedProperties);
+    }
+    this.getGEOIPData();
+  }
+
+  async getGEOIPData() {
+    const IPClass = new UserIP();
+    const userIPData = await IPClass.updateUserIP();
+    return fetch(this.locationEndpoint + userIPData.ip)
+      .then(resp => {
+        if (resp.ok) {
+          return resp.json();
+        }
+        return false;
+      })
+      .then(data => {
+        console.log(data);
+        this.lat = data.latitude;
+        this.long = data.longitude;
+        this.city = data.city;
+        this.state = data.region_name;
+        return data;
+      });
+  }
+
+  static get styles() {
+    return [
+      css`
+        :host {
+          display: block;
+        }
+        iframe {
+          height: 500px;
+          width: 500px;
+        }
+      `,
+    ];
+  }
+
+  render() {
+    // this function runs every time a properties() declared variable changes
+    // this means you can make new variables and then bind them this way if you like
+    const url = `https://maps.google.com/maps?q=${this.lat},${this.long}&t=&z=15&ie=UTF8&iwloc=&output=embed`;
+    return html`<iframe title="Where you are" src="${url}"></iframe>
+      <ul>
+        <a href="https://www.google.com/maps/@${this.lat},${this.long},14z">
+          Open in Google Maps
+        </a>
+      </ul>
+      <script>
+        window.__appCDN = 'https://cdn.webcomponents.psu.edu/cdn/';
+      </script>
+      <script src="https://cdn.webcomponents.psu.edu/cdn/build.js"></script>
+      <wikipedia-query search="${this.city}, ${this.state}"></wikipedia-query>
+      <wikipedia-query search="${this.city}"></wikipedia-query>
+      <wikipedia-query search="${this.state}"></wikipedia-query> `;
+  }
+}
+
+customElements.define(LocationFromIP.tag, LocationFromIP);
